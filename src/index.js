48c2a689fcb050f1184acc3525d8b0749da38d52
bhavayAnand9 <bhavayanandcse@gmail.com>
2018-12-28 19:23:03 +0530

    sass added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/style.scss";
 
 ReactDOM.render(<h1>Hello world!!</h1>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.s?css/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
