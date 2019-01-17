3123a2a0d2a03b62fbeadf518d27809018005c29
Ankit Tewari <46291173+4nkt@users.noreply.github.com>
2019-01-17 23:51:10 +0530

    Add styles using sass

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const HelloWorld = () => <h1>Hello, world!</h1>;
+const HelloWorld = () => <h1 className="blue">Hello, world!</h1>;
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from "./components/hello-world";
+import "./stylesheets/styles.scss";
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
